d2e8fcc28d1ee744f94d43e3134e456e487128b3
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-01-01 18:35:15 -0600

    Rename form to user and add ids to inputs.

---
+++ views/user.php
@@ -0,0 +1,20 @@
+<form method="POST">
+    <div class="field-group">
+        <label for="username">Username:</label>
+        <input id="username" type="text" name="username"/>
+    </div>
+
+    <div class="field-group">
+        <label for="firstName">First Name:</label>
+        <input id="firstName" type="text" name="firstName"/>
+    </div>
+
+    <div class="field-group">
+        <label for="lastName">Last Name:</label>
+        <input id="lastName" type="text" name="lastName"/>
+    </div>
+
+    <div class="field-group">
+        <input type="submit" value="Create User"/>
+    </div>
+</form>
